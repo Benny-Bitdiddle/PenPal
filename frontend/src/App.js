4f172cf4112dd618d40f5e81b18a19f9e831670a
kavantan <kavantan10@gmail.com>
2023-04-06 15:34:32 +0800

    Implement stylings for App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
+import React, { useState } from "react";
 import InputForm from "./components/InputForm";
 import SummaryDisplay from "./components/SummaryDisplay";
 import "./App.css";
 
 function App() {
+  const [summary, setSummary] = useState("");
+
   return (
-    <div>
-      <InputForm />
-      {/* <SummaryDisplay /> */}
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">PenPal</h1>
+      </header>
+      <main>
+        <InputForm setSummary={setSummary} />
+        <SummaryDisplay summary={summary} />
+      </main>
     </div>
   );
 }
